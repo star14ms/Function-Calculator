522a8b405509fec8d39d94fcacf77cdcebed3e98
star14ms <68639278+star14ms@users.noreply.github.com>
2020-08-14 02:43:57 +0900

    Update function.py

--- function.py
+++ function.py
@@ -1,43 +1,64 @@
 from functions import *
 
-def f(x): # x의 값이 먼저 정의되어 인자로 받을 수 있어야 함
-  print('ㅡ' * 16)
-  alpha = 'abcdefghijklmnopqrstuvwxyz' # 25차식까지 지원 (a, b, c : ax^2 + bx + c)
+def fx(list):
+  if isTrue_rational_num_list(list) == False: # 유리수가 아니면 초기화
+    return None
+  coefficients = Change_strs_fractions(list) # 유리수면 분수꼴로 변환
+  coefficients.reverse() # 오름차순으로 (계산, 출력 편의)
+  return coefficients
 
-  print('Type the coefficients for each term in descending order') # (각 항의 계수를 내림차순으로 입력해라)
-  terms = input('Coefficients : ').split() # (Coefficients: 계수)
-  if isTrue_rational_num_list(terms) == False: # 유리수가 아니면 초기화
-    return 'error: Not rational num!'
-  terms = Change_strs_fractions(terms) # 유리수면 분수꼴로 변환
-
-  if len(terms) == 1:
-    print('f(x) = ', end='')
-    y = Change_to_decimal(terms[0]) # 가능하면 분수->소수 전환
-    return y # 상수항 뿐일때 답 바로 반환
-
-  for n in range(len(alpha)-len(terms)):
-    terms.insert(0, 0) # 나머지 항은 값이 0
-  terms.reverse() # 오름차순으로 (계산, 출력 편의)
-
-  print('f(x) = ', end='')
-  Print_human_tailored_expansion(terms) # (인간맞춤형 식 출력)
-
-  print('     = ', end='')
+def f(x, coefficients): ### x, Coefficients : rational-num ###
   y = 0
-  for n in range(len(terms)):
+  for n in range(len(coefficients)):
     if n == 0:
-      y += terms[0]
+      y += coefficients[0]
     else:
-      y += terms[n]*x**n
-  y = Change_to_decimal(y)
-  return y # 답 계산하여 반환
+      y += coefficients[n]*x**n  
+  return y
 
+
+# 사용법 익히기
+
+print('ㅡ' * 20)
+print('1. define f(x):') 
+print("Type 'fx' or 'f(x)' and type coefficients\nfor each Coefficients in descending order\n")
+# 1. f(x) 정의: 'f(x)'를 치고, 각 항의 계수를 내림차순으로 입력해라
+print('2. define value of x')
+print('Type fx or f(x) with a substitute of\nsome rational-num in x')
+print('ex) f1.7, f(1/3)')
+# 2. x의 값 정의: x자리에 어떤 유리수를 대입하여 f(x)를 한번 더 쳐라
+
+coefficients = None
 while True:
-  print('ㅡ' * 16)
+  print('ㅡ' * 20)
+  a = input()
+  
+  if a == 'fx' or a == 'f(x)':
+    while True:
+      list = input('Coefficients(rational-num): ').split() # Coefficients: 계수
+      coefficients = fx(list)
+  
+      if coefficients == None: 
+        print('Error: not rational-num!'); continue # 유리수가 아닌 원소가 있었으면 다시
+      else: break
 
-  x = input('x = ') # x의 값 입력받고, 
-  if isTrue_rational_num(x) == False: # 유리수가 아니면 초기화
-    print('error: Not rational num!'); continue
-  x = Change_str_fraction(x) # 유리수면 분수꼴로 변환
+    expression = Change_human_tailored_expression(coefficients) # 인간맞춤형 x에 대한 식으로 변환
+    print('f(x) =', expression); continue # 식 출력
 
-  print(f(x))
+  if coefficients == None:
+    print("Type 'fx' or 'f(x)' first!"); continue
+  
+  if 'f' in a and a.index('f')==0:
+    if 'f(' in a and ')' in a and a.index('f(')==0 and a.index(')')==len(a)-1:
+      x = a.replace('f(','').replace(')','')
+    else: 
+      x = a.replace('f','')
+  
+    if isTrue_rational_num(x) == False: # 유리수가 아니면 다시
+      print('Error: not rational-num!'); continue
+    x = Change_str_fraction(x) # 유리수면 분수꼴로 변환
+
+    print('f(x) =', expression); # 식 출력
+    
+    print('f({}) ='.format(Change_to_decimal(x)), Change_to_decimal(f(x, coefficients))) # 값 계산하여 출력
+  else: print("Type f{num}, or f({num})!"); continue
